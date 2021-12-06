4154a54972e35961f7c6a16e732ab1ee35176b12
snowtothemax <mkjohnson9@wisc.edu>
2021-12-05 19:39:58 -0600

    Finished adress gen

--- FFTFiles/address_generator_tb.sv
+++ FFTFiles/address_generator_tb.sv
@@ -3,8 +3,8 @@ module address_generator_tb();
     logic [4:0] stageCount;
     logic [8:0] cycleCount, cycleCount_spec;
     logic [9:0] indexA, indexB, indexB_exp, indexA_exp;
-    logic [8:0] twiddleIndex;
-    integer bucket, bucketsMAX, cycleLimit, startingPos, bucketIterationCnt;    // bucket refers to which DFT the cycle iteration refers to
+    logic [8:0] twiddleIndex, twiddleIndex_int, twiddleIndex_exp;
+    integer bucket, bucketsMAX, cycleLimit, startingPos, bucketIterationCnt, dftSize;    // bucket refers to which DFT the cycle iteration refers to
     logic clk;
 
     address_generator iDUT(.*);
@@ -13,8 +13,19 @@ module address_generator_tb();
 
     assign bucketsMAX = 2 ** stageCount;
     assign cycleLimit = 512 / bucketsMAX;
-    assign startingPos = bucket == 0 ? bucket : (1024 / (2 ** (bucketsMAX - bucket)));
+    assign dftSize = 1024 / bucketsMAX;
+    assign startingPos = bucket * dftSize;
     assign indexB_exp = indexA + (512 >> stageCount);
+    assign twiddleIndex_int = $floor(cycleCount * (2 ** (stageCount + 1)) / 1024);
+    assign twiddleIndex_exp = {twiddleIndex_int[0], 
+                           twiddleIndex_int[1],
+                           twiddleIndex_int[2],
+                           twiddleIndex_int[3],
+                           twiddleIndex_int[4],
+                           twiddleIndex_int[5],
+                           twiddleIndex_int[6],
+                           twiddleIndex_int[7],
+                           twiddleIndex_int[8]};
 
     initial begin
         clk = 0;
@@ -38,24 +49,34 @@ module address_generator_tb();
             end
         end
 
-        // check stage 1
-        stageCount = 1;
-        cycleCount = 0;
+        for (stageCount = 0; stageCount < 10; stageCount ++) begin
+            cycleCount = 0;
+            // should go through each "Bucket" and compute the correct indices.  
+            for (bucket = 0; bucket < bucketsMAX && cycleCount < 512; bucket++) begin
+                for(bucketIterationCnt = 0; bucketIterationCnt < cycleLimit; bucketIterationCnt++) begin
 
-        // should go through each "Bucket" and compute the correct indices.  
-        for (bucket = 0; bucket < bucketsMAX && cycleCount < 511; bucket++) begin
-            for(bucketIterationCnt = 0; bucketIterationCnt < cycleLimit; bucketIterationCnt++) begin
+                    indexA_exp = startingPos + bucketIterationCnt;
 
-                indexA_exp = startingPos + bucketIterationCnt;
+                    @(posedge clk);
 
-                @(posedge clk);
+                    if (indexA != (startingPos + bucketIterationCnt)) begin
+                        $display("ERROR: Index A: %d Expected: %d", indexA, indexA_exp);
+                        $stop();
+                    end
 
-                if (indexA != (startingPos + bucketIterationCnt)) begin
-                    $display("ERROR: FUCK YOU. EAT CUM");
-                    $stop();
+
+                    if (indexB != indexB_exp) begin
+                        $display("ERROR: Index B: %d Expected: %d", indexB, indexB_exp);
+                        $stop();
+                    end
+
+                    if (twiddleIndex != twiddleIndex_exp) begin
+                        $display("ERROR: Twiddle Index: %d Expected: %d", twiddleIndex, twiddleIndex_exp);
+                        $stop();
+                    end
+
+                    cycleCount++;
                 end
-
-                cycleCount++;
             end
         end
 
