e765ae35da5a0a7284223611e5c3fd26bc20338e
mrreider <rbrostoff@wisc.edu>
2021-12-07 16:49:34 -0600

    a '

--- class/src/mips.cpp
+++ class/src/mips.cpp
@@ -285,7 +285,7 @@ namespace priscas
 			w = (w.AsUInt32() | ((signum & ((1 << 18) - 1) ) << 9 ));
 			w = (w.AsUInt32() | ((op & ((1 << 5) - 1) ) << 27 ));
 		}
-		printf("w:%x",w)
+		printf("w:%x",w);
 		return w;
 	}
 
