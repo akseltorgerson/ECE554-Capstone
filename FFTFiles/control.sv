f9ce080ec0f958c819b17cf2622c5c7f65082f31
akeehbler <akeehbler@wisc.edu>
2021-12-09 17:02:31 -0600

    framework for control unit

--- FFTFiles/control.sv
+++ FFTFiles/control.sv
@@ -1,9 +1,11 @@
 module control(
     //Inputs
-    startF, startI, loadF, sigNum, filter, done
+    startF, startI, loadF, sigNum, doFilter, done, clk, rst
     //Outputs
-    fftCalculating, storeSigNum
+    fftCalculating, storeSigNum, writeFilter, isIFFT, fDone, 
 );
+
+    input clk, rst;
 
     //Control signal from CPU to let accel know to do an FFT
     input startF;
@@ -29,8 +31,15 @@ module control(
     //Singal to determine that the sigNum should be stored in it's register
     output storeSigNum;
 
+    output fDone;
 
+    output isIFFT;
 
+    always @(*) begin
+        case()
+
+        endcase
+    end
 
 
 
