f3581c4612a48d3c3896f6bf042e188b39d6c57a
akeehbler <akeehbler@wisc.edu>
2021-12-12 00:40:10 -0600

    more on processor test bench

--- PROCFiles/proc_tb.sv
+++ PROCFiles/proc_tb.sv
@@ -24,7 +24,24 @@ module proc_tb();
                     .cv_value(cv_value_actual));
 
     initial begin
-        
+        clk = 1'b0;
+        rst = 1'b0;
+        //Expected outputs
+        op_expected = 2'b0;
+        common_data_bus_out_expected = 512'b0;
+        io_addr_expected = 32'b0;
+        cv_value_expected = 64'b0;
+        //Input stimulus
+        common_data_bus_in = 512'b0;
+        tx_done = 1'b0;
+        rd_valid = 1'b0;
+
+        //RESET
+        rst = 1'b1;
+        @(posedge clk);
+        @(negedge clk);
+        rst = 1'b0;
+        //START TESTING
     end
 
     always #5 clk = ~clk;
