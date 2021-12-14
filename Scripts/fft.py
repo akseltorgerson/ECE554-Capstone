a3da9600724dbbdae9602ba2d842de72e5949050
mrreider <rbrostoff@wisc.edu>
2021-12-14 11:54:05 -0600

    added time domain plot

--- Scripts/fft.py
+++ Scripts/fft.py
@@ -15,14 +15,30 @@ def plt_fft():
         if not file.endswith(".wav"):
             print(f'Arg {file} does not contain a ".wav" ending')
             continue
-        plt.figure()
         fs, data = wav.read(file)
+        filename = file[:-4]
+        duration = len(data)/fs
+        time = np.arange(0,duration,1/fs) # time vector
+        
+        # Time domain plot
+        plt.figure()
+        plt.plot(time,data)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Amplitude')
+        plt.title(f'Time domain of {filename}')
+        plt.savefig(f'figures/{filename}_Time')
+        plt.close()
+
+        # Freq domain after fft
+        plt.figure()
         data = data.T[0]
         yf = fft(data)
         xf = fftfreq(len(data), 1 / fs)
         plt.plot(xf, np.abs(yf))
-        filename = file[:-4]
-        plt.savefig(f'figures/{filename}')
+        plt.xlabel('Frequency Hz')
+        plt.ylabel('Amplitude')
+        plt.title(f'Frequency domain of {filename}')
+        plt.savefig(f'figures/{filename}_FFT_Frequency')
         plt.close()
 
 if __name__ == "__main__":
