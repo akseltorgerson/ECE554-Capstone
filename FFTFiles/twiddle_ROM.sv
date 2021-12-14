fa1b26a29ec4a187d0ff99e4e6875273655654cb
akseltorgerson <akseltorgerson@gmail.com>
2021-12-13 20:20:53 -0600

    twiddle deez nuts

--- FFTFiles/twiddle_ROM.sv
+++ FFTFiles/twiddle_ROM.sv
@@ -1,19 +1,16 @@
 module twiddle_ROM(
     input clk,
     input [8:0] twiddleIndex,
-    output reg [31:0] twiddle_real, twiddle_imag
+    output [31:0] twiddle_real, twiddle_imag
 );
 
     reg [31:0] twiddle_mem [0:1023];
-    integer i;
+    
+    assign twiddle_real = twiddle_mem[2*twiddleIndex];
+    assign twiddle_imag = twiddle_mem[2*twiddleIndex + 1];
 
     initial begin
         $readmemh("twiddleHex.mem", twiddle_mem);
-    end
-
-    always_ff @(posedge clk) begin
-        twiddle_real <= twiddle_mem[2*twiddleIndex];
-        twiddle_imag <= twiddle_mem[2*twiddleIndex + 1];
     end
     
 endmodule
